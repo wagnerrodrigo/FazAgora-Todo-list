825e5459fc6055ba22049f8a43bf9b712392800a
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-09-29 17:42:24 -0300

    Inicio do projeto - add classpath io.realm - add plugin realm-android - add implementation io.realm

---
+++ app/src/main/java/com/example/wagner/fazagora_todo_list/bemVindoActivity.java
@@ -0,0 +1,13 @@
+package com.example.wagner.fazagora_todo_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bemVindoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+    }
+}
