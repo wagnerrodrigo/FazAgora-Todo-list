004c0401c12f1ffbb7c64bd795f2910a4e932966
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-09-29 18:09:48 -0300

    class Constante

---
+++ app/src/main/java/com/example/wagner/fazagora_todo_list/Constantes.java
@@ -0,0 +1,7 @@
+package com.example.wagner.fazagora_todo_list;
+
+public class Constantes {
+    private static final String INSTANCE_ADDRESS = "foo.us1.cloud.realm.io";
+    static final String AUTH_URL = "https://" + INSTANCE_ADDRESS + "/auth";
+    static final String REALM_BASE_URL = "realms://" + INSTANCE_ADDRESS;
+}
