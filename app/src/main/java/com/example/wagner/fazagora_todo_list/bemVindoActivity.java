0bce3c429fcb2aade99d331c80911eec15653bb4
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-09-29 20:51:26 -0300

    fim das modificações class principal

--- app/src/main/java/com/example/wagner/fazagora_todo_list/bemVindoActivity.java
+++ app/src/main/java/com/example/wagner/fazagora_todo_list/bemVindoActivity.java
@@ -1,7 +1,13 @@
 package com.example.wagner.fazagora_todo_list;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +16,7 @@ import io.realm.ObjectServerError;
 import io.realm.SyncCredentials;
 import io.realm.SyncUser;
 
-import static com.example.wagner.fazagora_todo_list.bemVindoActivity.Constants.AUTH_URL;
+import static com.example.wagner.fazagora_todo_list.Constantes.AUTH_URL;
 
 public class bemVindoActivity extends AppCompatActivity {
 
@@ -25,14 +31,14 @@ public class bemVindoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bem_vindo);
 
         if(SyncUser.currentUser() !=null){
-            this.goToItemsActivity();
+            //this.goToItemsActivity();
 
         }
 
         // Setar o formulario de log.
         mNicknameTextView = findViewById(R.id.nickname);
         Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(view -> attemptLogin());
+       // loginButton.setOnClickListener(view -> attemptLogin());
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
@@ -41,12 +47,48 @@ public class bemVindoActivity extends AppCompatActivity {
         // resetar erros
         mNicknameTextView.setError(null);
         // armazenar valores de tentativas de login
-
+        String nickname = mNicknameTextView.getText().toString();
+        showProgress(true);
+        SyncCredentials credentials;
+        credentials = SyncCredentials.nickname(nickname, true);
+        SyncUser.loginAsync(credentials, AUTH_URL, new SyncUser.Callback<SyncUser>() {
+            @Override
+            public void onSuccess(SyncUser user) {
+                showProgress(false);
+               // goToItemsActivity();
+            }
+            @Override
+            public void onError(ObjectServerError error) {
+                showProgress(false);
+                mNicknameTextView.setError("Ah ouh um erro aconteceu! (verifique seu login por favor)");
+                mNicknameTextView.requestFocus();
+                Log.e("Login error", error.toString());
+            }
+        });
     }
 
-
-
-
-
-
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        });
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        mProgressView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+    //    private void goToItemsActivity(){
+//        Intent intent = new Intent(bemVindoActivity.this, ItemsActivity.class);
+//        startActivity(intent);
+//    }
 }
