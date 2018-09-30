ba7e5285aa90814efb9b6330d4c8de5e7318c3d2
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-09-30 10:56:48 -0300

    metodos criados

--- app/src/main/java/com/example/wagner/fazagora_todo_list/Model/Item.java
+++ app/src/main/java/com/example/wagner/fazagora_todo_list/Model/Item.java
@@ -19,5 +19,36 @@ public class Item extends RealmObject {
 
     @Required private Date timestamp;
 
+    public String getItemId() {
+        return itemId;
+    }
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public Boolean getIsDone() {
+        return isDone;
+    }
+    public void setIsDone(Boolean done) {
+        isDone = done;
+    }
+    public Date getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+    public Item() {
+        this.itemId = UUID.randomUUID().toString();
+        this.body = "";
+        this.isDone = false;
+        this.timestamp = new Date();
+    }
+
 
 }
