c9561a5dcad2b8071c7ac23ca549ab0a957d52f9
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2018-09-30 10:33:10 -0300

    importação, extends RealmOject, variaveis

--- app/src/main/java/com/example/wagner/fazagora_todo_list/Model/Item.java
+++ app/src/main/java/com/example/wagner/fazagora_todo_list/Model/Item.java
@@ -1,4 +1,23 @@
 package com.example.wagner.fazagora_todo_list.Model;
 
-public class Item {
+import java.util.Date;
+import java.util.UUID;
+
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Item extends RealmObject {
+    @PrimaryKey
+    @Required
+    private String itemId;
+
+    @Required private String body;
+
+    @Required private Boolean isDone;
+
+    @Required private Date timestamp;
+
+
 }
